ed4884f00655af1304f6fb68f7304e1b9695b3b5
Jim V <vlandham@gmail.com>
2015-02-27 15:01:51 -0800

    initial films view

--- src/js/pages/film/film-page.js
+++ src/js/pages/film/film-page.js
@@ -2,6 +2,9 @@ define(function(require) {
 
   var View = require('../../core/view');
   var template = require('tmpl!../film/film-page');
+  var ThumbnailView = require('../../pages/film/film-tile');
+  var DetailView = require('../../pages/film/film-detail-header');
+  var Promise = require('bluebird');
 
   return View.extend({
 
@@ -9,11 +12,27 @@ define(function(require) {
 
     initialize: function(options) {
       this.options = options;
+      this.views = {};
     },
 
     _render: function() {
+      var self = this;
       this.$el.html(this.template(this.options));
-      return this;
+
+      // thumbnail view
+      var thumbnailView = new ThumbnailView({ film_id : this.options.film_id });
+
+      // header view
+      var detailView = new DetailView({ film_id : this.options.film_id });
+
+      this.views['tile'] = thumbnailView;
+      this.views['details'] = detailView;
+
+      return Promise.join(thumbnailView.render(), detailView.render(), function(t_view, h_view) {
+        self.$el.find('.film-tile-container').append(t_view.$el);
+        self.$el.find('.film-detail-container').append(h_view.$el);
+        return self;
+      });
     }
   });
 });
