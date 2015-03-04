d069ac376386fef1566a1dbf359f95919dbc611f
Yannick Assogba <yassogba@gmail.com>
2015-03-04 18:05:19 -0500

    [Adjective Page] Move responsibility for rendering trope details out from to the vis to the page.
    Use the trope-tile and trope details views from the trope folder. Some general cleanup and docs.

--- src/js/pages/adjectives/adjectives-page.js
+++ src/js/pages/adjectives/adjectives-page.js
@@ -1,20 +1,24 @@
 define(function(require) {
 
   var $ = require('jquery');
+  var _ = require('lodash');
+  var Promise = require('bluebird');
 
   var View = require('../../core/view');
   var template = require('tmpl!../adjectives/adjectives-page');
   var DataManager = require('../../data/data_manager');
 
   var AdjectiveVis = require('./adjectives-vis');
-  console.log('AdjectiveVis', AdjectiveVis);
+
+  var TropeTile = require('../trope/trope-tile.js');
+  var TropeDetails = require('../trope/trope-detail-header');
 
   return View.extend({
 
     template: template,
 
     initialize: function(options) {
-      console.log('adj page initialize');
+
     },
 
     _render: function() {
@@ -22,6 +26,11 @@ define(function(require) {
 
       //Set up the initial template
       this.$el.html(this.template());
+
+      // Save the original blurb for the page. It will
+      // be replaced with trope descriptions at various
+      // points and should be restorable.
+      this.originalBlurb = $('.blurb').html();
 
       //Load data and render the visualization
       return DataManager.getAdjectiveNetwork().then(function(adjectiveData){
@@ -31,15 +40,54 @@ define(function(require) {
           container: self.$el.find('.adjectives-page .canvas').get(0)
         });
 
+        adjVis.on('tropeSelected', function(tropeId){
+          self.showTropeDetails(tropeId);
+        });
+
         $(window).resize(function(){
           adjVis.update();
           adjVis.render();
         });
 
-        window.adjVis = adjVis;
-        // return adjVis.render();
+        return adjVis.render();
       });
+    },
 
+    /**
+     * Show details for a currently selected trope in the area where
+     * the page introduction /description was.
+     *
+     * Restore the original description is trope is deselected.
+     * @param  {String} tropeId
+     */
+    showTropeDetails: function(tropeId) {
+      var self = this;
+
+      if(_.isNull(tropeId)){
+        var el = $('.blurb');
+        if(self.tile && self.tropeDesc){
+          Promise.settle([self.tile.remove(), self.tropeDesc.remove()]).then(function(){
+            el.html(self.originalBlurb);
+          });
+        }
+      } else {
+        DataManager.getTropeDetails(tropeId).then(function(details){
+          var el = $('.blurb');
+
+          self.tile = new TropeTile({ trope_id : tropeId });
+          self.tropeDesc = new TropeDetails({ trope_id : tropeId });
+
+          // var pp = self.tile.render();
+          // console.log('trope details promise', pp)
+          Promise.settle([self.tile.render(), self.tropeDesc.render()]).then(function(){
+            el.empty();
+            el.append(self.tile.$el);
+            el.append(self.tropeDesc.$el);
+          });
+
+        });
+      }
     }
+
   });
 });
--- src/js/pages/adjectives/adjectives-vis.js
+++ src/js/pages/adjectives/adjectives-vis.js
@@ -1,44 +1,75 @@
 define(function(require) {
 
+  var Backbone = require('backbone');
   var _ = require('lodash');
   var d3 = require('d3');
   var dataManager = require('../../data/data_manager');
-  var tropeBlurbTmpl = require('tmpl!../adjectives/trope-blurb');
 
+
+  /**
+   * The main visualization for the adjectives page.
+   *
+   * This visualization renders the top N (100) adjectives in a ring
+   * connecting adjectives that co-occur together with arcs running through
+   * the center.
+   *
+   * It also allows the user to select an adjective, when that happens,
+   * the tropes associated with that adjective are displayed in the center of the
+   * circle.
+   *
+   * @param {Object} options
+   * @param {Object} options.data
+   * @param {Object} options.container the dom element to render into
+   *
+   */
   function AdjectiveVis(options){
     var self = this;
+
     this.data = options.data;
     this._container = options.container;
+
+    // Holders for currenly selected adjectives and tropes
+    // to allow us to have locked selections, as well as
+    // mouseover interaction.
     this.currentlySelectedAdj = null;
     this.currentlySelectedTrope = null;
 
+    // The tropes associated with the currently selected adjectives.
     this.associatedTropes = [];
+    // Map of adjectives to tropes
+    this.adjToTrope = _.reduce(this.data.trope_adj_network.links, function(memo,link){
+      if(_.isUndefined(memo[link.target])){
+        memo[link.target] = [];
+      }
+      memo[link.target].push(link.source);
+      return memo;
+    }, {});
 
 
-    this.update();
+    self.update();
+    self.init();
 
-    // Prime the trope info cache for quick access later
-    dataManager.getTrope("").then(function(){
-      self.init();
+    dataManager.getTropes().then(function(tropes){
+      self.tropeInfo = _.reduce(tropes, function(memo, ti){
+        memo[ti.id] = ti;
+        return memo;
+      }, {});
+
       self.render();
     });
 
-    // TODO: consider whether this should live outside this component
-    // for now i'm putting the logic to update the blurb here to test
-    // it out.
-    this.originalBlurb = d3.select('.blurb').html();
-    console.log(this.originalBlurb);
+    // Add event support to this object so that we can alert
+    // external components of when a trope is selected.
+    _.extend(this, Backbone.Events);
   }
 
-  AdjectiveVis.prototype.getTropes = function(adjective) {
-    var tropes = this.adjToTrope[adjective];
-    if(_.isUndefined(tropes)){
-      return [];
-    } else {
-      return tropes;
-    }
-  };
 
+  /**
+   * Update scales and metrics in preparation for rendering.
+   *
+   * Should be called before an initial render or if the data
+   * or dimensions associated with the visualization change.
+   */
   AdjectiveVis.prototype.update = function() {
     var self = this;
     this.container = d3.select(this._container);
@@ -71,7 +102,7 @@ define(function(require) {
 
     // Prepare the node and link data.
 
-    // Find a given node by name in the adjective-adjective tree.
+    // Finds a given node by name in the adjective-adjective tree.
     function findNode(root, name) {
       return _.find(root.children, function(node){
         return node.name === name;
@@ -94,24 +125,19 @@ define(function(require) {
       };
     });
 
+    // We filter out links that connect adjectives whose appearance together in
+    // tropes is below a certain threshold.
     this.links = this.links.filter(function(link) {
       return link.weight > 10;
     });
 
-    // Prepare adj to trope data
-
-    this.adjToTrope = _.reduce(this.data.trope_adj_network.links, function(memo,link){
-      if(_.isUndefined(memo[link.target])){
-        memo[link.target] = [];
-      }
-      memo[link.target].push(link.source);
-      return memo;
-    }, {});
-
-
-
   };
 
+  /**
+   * Initial rendering function. Expected to be called
+   * only once. Adds basic elements to the container.
+   *
+   */
   AdjectiveVis.prototype.init = function() {
     var svg = this.container.append('svg')
       .attr('height', this.height)
@@ -132,7 +158,13 @@ define(function(require) {
     this.adjAdjNode = g.append("g");
   };
 
+
+  /**
+   * Render the visualization.
+   *
+   */
   AdjectiveVis.prototype.render = function() {
+    // Adjust dimensions in case things have been resized.
     this.container.select('svg')
       .attr('height', this.height)
       .attr('width', this.width);
@@ -142,22 +174,32 @@ define(function(require) {
 
     this.renderAdjAdjNetwork();
     this.renderLinkedTropes();
-    this.showTropeDetails();
   };
 
 
+  /**
+   * Render the adjective to adjective network, this is comprised
+   * of a 'ring' of adjectives connected by arcs indicating which ones
+   * co-occur.
+   *
+   * It uses an edge bundling algorithm that expects a tree of nodes
+   * so we place all our actual nodes (the adjectives)
+   * under an invisible root node.
+   *
+   */
   AdjectiveVis.prototype.renderAdjAdjNetwork = function() {
     var self = this;
 
     var bundle = d3.layout.bundle();
-
     var line = d3.svg.line.radial()
       .interpolate('bundle')
       .tension(0.85)
       .radius(function(d) { return d.y; })
       .angle(function(d) { return d.x / 180 * Math.PI; });
 
-    //Render Links
+    //
+    // Render Links
+    //
     var link = this.adjAdjLink.selectAll(".link")
         .data(bundle(this.links), function(link) {
           return _.pluck(link, "name").join("");
@@ -173,53 +215,59 @@ define(function(require) {
       })
       .attr("d", line);
 
-    //Render Nodes
+    //
+    // Render Nodes
+    //
+    // We first append the all the elements that make up a node. Then
+    // later update them with rendering attributes.
+    //
     var node = this.adjAdjNode.selectAll(".node")
       .data(this.nodes.filter(function(n) { return !n.children; }));
 
     var nodeEnter = node.enter()
       .append("g")
       .attr("class", "node")
-      .on("mouseover", mouseoverHelper)
-      .on("mouseout", mouseoutHelper)
+      .on("mouseover", mouseovered)
+      .on("mouseout", mouseouted)
       .on("click", mouseclicked);
 
+    // This rect is an invisible one to capture mouse gestures over
+    // a larger region than is actually by the other discrete elements
+    // in the group.
     nodeEnter.append("rect")
       .attr("class", "node-mouse");
 
     nodeEnter.append("text")
       .attr("class", "node-text");
 
+    // The adjective occurrence bar
     nodeEnter.append("rect")
       .attr("class", "node-rect");
 
+    // The numeric annotation for the occurrence bar.
     nodeEnter.append("text")
       .attr("class", "node-text count");
 
 
-    // Overall group for the node elements
+    // Start updating the elements of a node with their current
+    // rendering attributes.
+
     node
       .attr("transform", function(d) {
         return "rotate(" + (d.x - 90) + ")translate(" + (d.y + 8) + ",0)";
       });
 
-
-    // An invisible rect that is there to capture mouse events.
     node.selectAll('rect.node-mouse')
       .attr("width", "200px")
       .attr("height", function(d, i){
-        d._rectHeight = 12;
-        return d._rectHeight;
+        d._mouseRectHeight = 12;
+        return d._mouseRectHeight;
       })
       .attr("x", "0px")
-      .attr("y", function(d){
-        return -d._rectHeight/2;
-      })
+      .attr("y", function(d){ return -d._mouseRectHeight/2; })
       .attr('fill', 'white')
       .attr('stroke', 'none');
 
-
-    // The adjective text
     node.selectAll('text.node-text')
       .attr("dy", ".31em")
       .attr("transform", function(d) {
@@ -229,14 +277,9 @@ define(function(require) {
       .text(function(d) { return d.name; });
 
 
-    // The adjective occurrence bar
     var barOffset = 100;
     node.selectAll('rect.node-rect')
-      .attr("width", function(d, i){
-        var w = self.rectScale(d.trope_count);
-        d._rectWidth = w;
-        return d._rectWidth;
-      })
+      .attr("width", function(d, i){ return self.rectScale(d.trope_count); })
       .attr("height", function(d, i){
         d._rectHeight = 6;
         return d._rectHeight;
@@ -249,13 +292,17 @@ define(function(require) {
       });
 
 
-    // The adjective occurrence count
     node.selectAll("text.count")
       .attr("transform", function(d) {
           return (d.x < 180 ? "" : "rotate(180)");
       })
       .attr("dy", ".31em")
       .attr("x", function(d, i){
+        // we position the count based on the
+        // size of the rect (bar) that precedes it.
+        // we also have to adjust for text anchor and positioning
+        // based on where along the circle it is so it does not
+        // look reversed.
         var sign = d.x < 180 ? 1 : -1;
         var rect = d3.select(this)[0][0].previousSibling;
         var bb = rect.getBBox();
@@ -265,79 +312,128 @@ define(function(require) {
       .text(function(d) { return d.trope_count; });
 
 
-    // Helper functions for interaction.
-    function mouseoverHelper(d) {
-      if(_.isNull(self.currentlySelectedAdj)){
-        mouseovered(d);
-      } else {
-        return;
-      }
-    }
 
+    //
+    //  Helper functions for interaction.
+    //
+
+
+
+    // Handle mouseover
+    //
+    // To support locked selections, it will check if there is a selected
+    // adjective before adjusting what should be highlighted. Thus this handler
+    // is stateful (depends on self.currentlySelectedAdj)
     function mouseovered(d) {
-      node.each(function(n) { n.target = n.source = false; });
-
-      link
-          .classed("link-source", function(l) {
-            if (l.target === d) { return l.source.source = true; }
-          })
-          .classed("link-target", function(l) {
-            if (l.source === d) { return l.target.target = true; }
-          })
-        .filter(function(l) { return l.target === d || l.source === d; })
-          .each(function() { this.parentNode.appendChild(this); }); // Render these links on top
-
-      node
-          .classed("node-target", function(n) { return n.target; })
-          .classed("node-source", function(n) { return n.source; });
-
-      node.classed("active", function(n) { return n.name === d.name; });
-    }
-
-    function mouseoutHelper() {
       if(_.isNull(self.currentlySelectedAdj)){
-        mouseouted();
+        node.each(function(n) { n.target = n.source = false; });
+
+        link
+            .classed("link-source", function(l) {
+              if (l.target === d) { return l.source.source = true; }
+            })
+            .classed("link-target", function(l) {
+              if (l.source === d) { return l.target.target = true; }
+            })
+          .filter(function(l) { return l.target === d || l.source === d; })
+            .each(function() { this.parentNode.appendChild(this); }); // Render these links on top
+
+        node
+            .classed("node-target", function(n) { return n.target; })
+            .classed("node-source", function(n) { return n.source; });
+
+        node.classed("active", function(n) { return n.name === d.name; });
+
       } else {
         return;
       }
     }
 
+
+    // Handle mouseout
+    //
+    // To support locked selections, it will check if there is a selected
+    // adjective before adjusting what should be unhighlighted. Thus this handler
+    // is stateful (depends on self.currentlySelectedAdj)
     function mouseouted() {
-      link
-        .classed("link-source", false)
-        .classed("link-target", false);
+      if(_.isNull(self.currentlySelectedAdj)){
+        link
+          .classed("link-source", false)
+          .classed("link-target", false);
 
-      node
-        .classed("node-target", false)
-        .classed("node-source", false);
+        node
+          .classed("node-target", false)
+          .classed("node-source", false);
 
-      node.classed("active", false);
+        node.classed("active", false);
+      } else {
+        return;
+      }
     }
 
+    // Handle clicking on adjectives. Basically uses the mouseout and mouseover
+    // functions to lock a selection and highlight it.
     function mouseclicked(d) {
+      mouseouted();
+      self.currentlySelectedAdj = null;
+      mouseovered(d);
       self.currentlySelectedAdj = d;
       self.render();
-      mouseouted(d);
-      mouseovered(d);
     }
 
-    this.bgRect
-      .on('click', clearAdjSelection);
 
-    function clearAdjSelection(){
+    // Dismiss selections
+    this.bgRect.on('click', clearSelection);
+    function clearSelection(){
       self.currentlySelectedAdj = null;
-      mouseoutHelper();
+      self.currentlySelectedTrope = null;
+      mouseouted();
+      self.trigger('tropeSelected', null);
       self.render();
     }
 
   };
 
+  /**
+   * Get tropes associated with a adjective. Basically a helper
+   * for the next function.
+   *
+   * @param  {String} adjective
+   * @return {Object} an array of trope information objects
+   *                  these have the same information as returned
+   *                  by DataManager.getTrope
+   */
+  AdjectiveVis.prototype.getTropes = function(adjective) {
+    var tropes = this.adjToTrope[adjective];
+    if(_.isUndefined(tropes)){
+      return [];
+    } else {
+      return tropes;
+    }
+  };
 
+
+  /**
+   * Render the tropes linked to a selected adjective.
+   *
+   * These are rendered using a force directed layout.
+   */
   AdjectiveVis.prototype.renderLinkedTropes = function() {
     var self = this;
 
+    // This is the maximum number of nodes at which we will
+    // render them full size.
     var maxFullSizeNodes = 65;
 
+    var nodeWidth = 85;
+    var nodeHeight = 20;
+
+    var nodeWidthSmall = 20;
+
+    // Adjust the currently selection of visible tropes
+    // because we may be going from one se to another and some
+    // may remain, we need to add new ones and remove old ones
+    // while keeping existing ones.
     if(_.isNull(this.currentlySelectedAdj)){
       this.associatedTropes = [];
     } else {
@@ -351,11 +447,13 @@ define(function(require) {
         }
       });
 
+      // We set the initial position of new nodes to random spots
+      // in a smallish window around the center of the vis.
       _.each(newTropes, function(tropeName){
         if(!_.include(oldTropes, tropeName)){
           self.associatedTropes.push({
-            width: 85,
-            height: 20,
+            width: nodeWidth,
+            height: nodeHeight,
             name: tropeName,
             x: -(Math.random() * 100) + (Math.random() * 100) + self.width / 2,
             y: -(Math.random() * 100) + (Math.random() * 100) + self.height / 2
@@ -370,6 +468,9 @@ define(function(require) {
 
     }
 
+    // I don't think this is working, but it is an attempt to better
+    // control where the nodes center around. So ignore it for now.
+    // TODO: Fix
     var data = this.associatedTropes;
     data.unshift({
       name: "",
@@ -379,6 +480,8 @@ define(function(require) {
       y: this.height / 2 - 20
     });
 
+    // The force layout, these parameters
+    // will need to be tweaked, so still a WIP.
     this.force = d3.layout.force()
       .gravity(0.55)
       // .linkDistance(100)
@@ -396,6 +499,8 @@ define(function(require) {
       })
       .size([this.width, this.height]);
 
+
+    // Start the force layout alg and then render the nodes.
     this.force.nodes(data);
     this.force.start();
 
@@ -404,9 +509,10 @@ define(function(require) {
     var nodes = svg.selectAll("g.tropeNode")
         .data(data.slice(1), function(t) { return t.name; });
 
-    var nodeEnter = nodes.enter();
 
-    var node = nodeEnter
+    // Append the nodes in the force layout.
+
+    var node = nodes.enter()
       .append("g")
       .attr("class", "tropeNode")
       .style('opacity', 1)
@@ -416,25 +522,29 @@ define(function(require) {
 
     node.append("rect")
       .attr("class", function(d, i) {
-        // TEMPORARY. Replace with a call in the constructor
-        // to getTropes so we can call this sync.
-        var info = dataManager.cache['tropes_basic'][d.name];
+        var info = self.tropeInfo[d.name];
         return "trope-node-bg " + (info.gender === 'm' ? 'male' : 'female');
       });
 
-
     node.append("text")
-      .attr("class", "trope-node-label")
+      .attr("class", function(d, i) {
+        var info = self.tropeInfo[d.name];
+        return "trope-node-label " + (info.gender === 'm' ? 'male' : 'female');
+      })
       .attr("dy", "14px")
       .attr("dx", "5px")
       .style("text-anchor", "start");
 
+    // Update the nodes and start them updating in a tick function
 
-    // Update the nodes
+    nodes.classed('active', function(d){
+      return d === self.currentlySelectedTrope;
+    });
+
     nodes.selectAll('rect.trope-node-bg')
       .attr("width", function(d) {
         if(data.length > maxFullSizeNodes) {
-          return 20;
+          return nodeWidthSmall;
         }
         return d.width;
       })
@@ -444,9 +554,9 @@ define(function(require) {
 
     nodes.selectAll('text.trope-node-label')
       .text(function(d) {
-        // TEMPORARY. Replace with a call in the constructor
-        // to getTropes so we can call this sync.
-        var name = dataManager.cache['tropes_basic'][d.name].name.toLowerCase();
+        var info = self.tropeInfo[d.name];
+        var name = info.name.toLowerCase();
+
         if(name.length > 10) {
           name = name.substring(0, 8) + '…';
         }
@@ -468,37 +578,42 @@ define(function(require) {
 
 
     this.force.on("tick", function(e) {
-      nodes
-          .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+      nodes.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
     });
 
 
+
+    //
+    // On mouse interactions with tropes we primarily trigger events for
+    // external handling.
+    //
+
     function tropeMouseoverHelper(d){
-      self.currentlySelectedTrope = d;
-      self.render();
+      if(_.isNull(self.currentlySelectedTrope)){
+        self.trigger('tropeSelected', d.name);
+        self.render();
+      } else {
+        return;
+      }
     }
 
     function tropeMouseoutHelper(d){
-      self.currentlySelectedTrope = null;
+      if(_.isNull(self.currentlySelectedTrope)){
+        self.trigger('tropeSelected', null);
+        self.render();
+      } else {
+        return;
+      }
+    }
+
+    function tropeMouseclicked(d){
+      self.currentlySelectedTrope = d;
+      self.trigger('tropeSelected', d.name);
       self.render();
     }
 
-    function tropeMouseclicked(){
-
-    }
-
   };
 
-  AdjectiveVis.prototype.showTropeDetails = function() {
-    if(_.isNull(this.currentlySelectedTrope)){
-      d3.select('.blurb').html(this.originalBlurb);
-    } else {
-      dataManager.getTropeDetails(this.currentlySelectedTrope.name).then(function(details){
-        var html = tropeBlurbTmpl(details);
-        d3.select('.blurb').html(html);
-      });
-    }
-  };
 
   return AdjectiveVis;
 
