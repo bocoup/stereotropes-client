cfc622954d8ecece8625ba1a7149c95192de9be1
Jim Vallandingham <vlandham@gmail.com>
2015-03-03 16:02:32 -0800

    start of list vis

--- src/js/pages/film/film-page.js
+++ src/js/pages/film/film-page.js
@@ -4,6 +4,7 @@ define(function(require) {
   var template = require('tmpl!../film/film-page');
   var ThumbnailView = require('../../pages/film/film-tile');
   var DetailView = require('../../pages/film/film-detail-header');
+  var TropesListView = require('../../pages/film/film-tropes-list');
   var Promise = require('bluebird');
 
   return View.extend({
@@ -25,12 +26,23 @@ define(function(require) {
       // header view
       var detailView = new DetailView({ film_id : this.options.film_id });
 
+      // tropes list view
+      var tropesListContainer = self.$el.find('.film-tropes-list-container');
+
+      var tropesListView = new TropesListView({ 
+        film_id : this.options.film_id,
+        width : tropesListContainer.width(),
+        height : 600
+      });
+
       this.views['tile'] = thumbnailView;
       this.views['details'] = detailView;
+      this.views['tropes'] = tropesListView;
 
-      return Promise.join(thumbnailView.render(), detailView.render(), function(t_view, h_view) {
+      return Promise.join(thumbnailView.render(), detailView.render(), tropesListView.render(), function(t_view, h_view, t_l_view) {
         self.$el.find('.film-tile-container').append(t_view.$el);
         self.$el.find('.film-detail-container').append(h_view.$el);
+        self.$el.find('.film-tropes-list-container').append(t_l_view.$el);
         return self;
       });
     }
---
+++ src/js/pages/film/film-tropes-list.js
@@ -0,0 +1,219 @@
+define(function(require) {
+  var Promise = require('bluebird');
+  var d3 = require("d3");
+  require("./d3.text");
+  var View = require("../../core/view");
+  var dataManager = require('../../data/data_manager');
+
+  var margin = {top:20, left:10, bottom:20, right:10};
+  var positions = {};
+
+  var textWrap = d3.svg.textWrap();
+
+  var tropeList = function() {
+    var height = 600;
+    var width = 600;
+    var minHeight = 240;
+    var textHeight = 30;
+    var data = [];
+    var g = null;
+
+    // var yScale = d3.scale.ordinal();
+    var xScale = d3.scale.ordinal();
+
+    var chart = function(selection) {
+      selection.each(function(rawData) {
+        data = processData(rawData);
+        setupScales(data);
+
+        var svg = d3.select(this).selectAll("svg").data([data]);
+        svg.enter().append("svg").append("g");
+
+        console.log(data);
+
+        svg.attr("width", width);
+        svg.attr("height", height);
+
+        g = svg.select("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        update();
+      });
+    };
+
+    function update() {
+      var genders = g.selectAll(".gender").data(data);
+      genders.enter().append("g")
+        .attr("class", function(d) { return "gender gender-" + d.key; })
+        .attr("transform", function(d,i) {
+          var shiftDown = (height - (textHeight * d.value.length)) / 2;
+          return "translate(" + positions[d.key] + "," + shiftDown + ")";
+        });
+
+      var tropes = genders.selectAll(".trope").data(function(d) { return d.value; } );
+      var tropesE = tropes.enter()
+        .append("g")
+        .attr("class", "trope");
+      tropesE.append("text")
+        .attr("transform", function(d,i) { 
+          return "translate(" + 0 + "," + i * textHeight+ ")";
+        })
+        .text(function(d) { return d.details.name; })
+        .on("mouseover", mouseover)
+        .on("mouseout", mouseout);
+    }
+
+    function mouseover(d,i) {
+      var text = d3.select(this);
+      text.classed('highlight', true);
+      // var bbox = text.node().getBBox();
+      var bbox = this.parentNode.getBBox();
+      
+      d3.select(text.node().parentNode)
+        .insert("rect", "text")
+        .attr("x", bbox.x - 4)
+        .attr("y", bbox.y)
+        .attr("width", bbox.width + 8)
+        .attr("height", bbox.height)
+        .attr("class", "underbox gender-" + d.details.gender);
+
+      var textX = positions.middle - (textWrap.bounds().width / 2);
+      var panel = g.selectAll('.middle-panel').data([d]);
+      var panelE = panel.enter().append("g")
+        .attr("class", "middle-panel")
+        .attr("transform", "translate(" + textX + "," + 0 + ")");
+
+      panelE.append("rect");
+      panelE.append("text")
+        .attr("text-anchor", "start");
+      panel.select("text").text(d.role)
+        .call(textWrap);
+  
+      bbox = panel.select("text").node().getBBox();
+      panel.select("rect")
+        .attr("class", "gender-" + d.details.gender)
+        .attr("x", bbox.x - 8)
+        .attr("y", bbox.y - 4)
+        .attr("width", bbox.width + 16)
+        .attr("height", bbox.height + 8);
+      
+    }
+
+    function mouseout(d,i) {
+      d3.select(this).classed('highlight', false);
+      d3.select(this.parentNode).select(".underbox").remove();
+    }
+
+    function setupScales(data) {
+      xScale.domain([0,0.5,1])
+        .rangeRoundPoints([0, width], 2);
+
+      positions = {
+        f : width / 4,
+        middle : (width / 2),
+        m : (width / 2) + (width / 4)
+      };
+      var maxLength = d3.max(data, function(d) { return d.value.length; });
+      height = Math.max(((maxLength * textHeight) + (margin.top + margin.bottom)), minHeight);
+      textWrap.bounds({width: width / 3, height: height, x:0,y:0}).padding(6);
+
+    }
+
+    function processData(rawData) {
+      var data = [];
+      if(rawData.roles) {
+        rawData.roles.f = rawData.roles.f || [];
+        rawData.roles.m = rawData.roles.m || [];
+        data = d3.entries(rawData.roles);
+        // ensure female is always first
+        data.sort(function(a,b) {
+          return (a.key === "f") ? -1 : 1;
+        });
+      }
+
+      return data;
+    }
+
+    chart.height = function(h) {
+      if (!arguments.length) {
+        return height;
+      }
+      height = h; //- (margin.top + margin.bottom);
+      return this;
+    };
+
+    chart.width = function(w) {
+      if (!arguments.length) {
+        return width;
+      }
+      width = w; // - (margin.left + margin.right);
+      return this;
+    };
+
+    return chart;
+  };
+
+  var plotData = function(selector, data, plot) {
+    return selector
+      .datum(data)
+      .call(plot);
+  };
+
+  var template = require("tmpl!../../pages/film/film-tropes-list");
+  /**
+   * Defines a view that wraps the above visualization
+   * @param  {Options} options Requires: height, width, film_id
+   * @return {Backbone.View}
+   */
+  return View.extend({
+    template: template,
+    initialize: function(options) {
+      this.film_id = options.film_id;
+      this.options = options;
+    },
+    _preDataRender: function() {
+      this.$el.html(this.template());
+      return this;
+    },
+    getRoleDetails: function(roles) {
+      var details = [];
+      // get details for all roles in the film
+      d3.keys(roles).forEach(function(g) {
+        roles[g].forEach(function(trope) {
+          details.push(dataManager.getTropeDetails(trope.id));
+        });
+      });
+      return details;
+    },
+    addRoleDetails: function(roles, roleDetails) {
+      var roleMap = d3.map(roleDetails, function(d) { return d.id; });
+      d3.keys(roles).forEach(function(g) {
+        roles[g].forEach(function(trope) {
+          trope.details = roleMap.get(trope.id);
+        });
+      });
+      return roles;
+    },
+    _render: function() {
+      var self = this;
+      self._preDataRender();
+      var list = tropeList();
+      list.width(self.options.width)
+        .height(self.options.height);
+      return dataManager.getFilmDetails(this.film_id).then(function(film_details) {
+
+        var roleDetails = self.getRoleDetails(film_details.roles);
+
+        return Promise.all(roleDetails).then(function(roleDetails) {
+          self.addRoleDetails(film_details.roles, roleDetails);
+
+          self.$el.html(self.template(film_details));
+          self.chart = d3.select(self.$el.find('.vis')[0]);
+          plotData(self.chart, film_details, list);
+          return self;
+        });
+
+      });
+    }
+  });
+});
