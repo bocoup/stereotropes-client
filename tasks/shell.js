01d76144d8fe1de1521a5960c19ca9db0a6117c4
Irene Ros <imirene@gmail.com>
2015-03-24 13:38:19 -0400

    making profile optional

--- tasks/shell.js
+++ tasks/shell.js
@@ -1,8 +1,9 @@
 module.exports = function(grunt) {
 
   var opts = {
-    // default aws profile is 'bocoup' but you can provide a diff one.
-    profile : grunt.option('profile') || 'bocoup',
+    // You need to provide a profile if you are going to use one, otherwise
+    // you should have a default profile setup.
+    profile : grunt.option('profile') ? ('--profile ' +  grunt.option('profile')) : '',
 
     // default bucket
     bucket: grunt.option('bucket') || "s3://gendertropes.bocoup.com",
@@ -18,7 +19,7 @@ module.exports = function(grunt) {
         execOpts: {
           maxBuffer: false
         },
-        cmd: grunt.template.process('aws --profile <%= profile %> s3 sync ' +
+        cmd: grunt.template.process('aws <%= profile %> s3 sync ' +
           'assets/data <%= bucket %>/assets/data --recursive --include \'*\' ' +
           '--acl \'public-read\' --cache-control <%= cacheControl %> ' +
           '--expires <%= expires %>' , { data : opts })
@@ -27,7 +28,7 @@ module.exports = function(grunt) {
         execOpts: {
           maxBuffer: false
         },
-        cmd: grunt.template.process('aws --profile <%= profile %> s3 sync '+
+        cmd: grunt.template.process('aws <%= profile %> s3 sync '+
           'public/ <%= bucket %> --exclude "*data/*" --recursive ' +
           '--acl \'public-read\' --cache-control <%= cacheControl %> ' +
           '--expires <%= expires %>' , { data : opts })
