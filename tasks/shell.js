395b22e54480f3c9b54590ac51dbf29ad3ea9462
Irene Ros <imirene@gmail.com>
2015-03-24 12:09:30 -0400

    Added s3 copy tasks: - You need to have the aws cli installed - You need to have credentials made for you (ask matt) - There are two tasks that will copy public (`grunt shell:public`) and   that will copy the assets/data (`grunt shell:data`).

---
+++ tasks/shell.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+
+  var opts = {
+    // default aws profile is 'bocoup' but you can provide a diff one.
+    profile : grunt.option('profile') || 'bocoup',
+
+    // default bucket
+    bucket: grunt.option('bucket') || "s3://gendertropes.bocoup.com",
+
+    // cache control
+    cacheControl: "max-age=3600000, public",
+    expires: new Date(Date.now() + 3600000).toUTCString()
+  };
+
+  grunt.config('shell', {
+      options: {
+        stderr: true
+      },
+      'data': {
+        command: grunt.template.process('aws --profile <%= profile %> s3 sync ' +
+          'assets/data <%= bucket %>/assets/data --recursive --include \'*\' ' +
+          '--acl \'public-read\' --cache-control <%= cacheControl %> ' +
+          '--expires <%= expires %>' , { data : opts })
+      },
+      "public": {
+        command: grunt.template.process('aws --profile <%= profile %> s3 sync '+
+          'public/ <%= bucket %> --exclude "*data/*" --recursive ' +
+          '--acl \'public-read\' --cache-control <%= cacheControl %> ' +
+          '--expires <%= expires %>' , { data : opts })
+      }
+  });
+
+  grunt.loadNpmTasks('grunt-shell');
+};
