0eb42a553f4f44c79a2a6348a0006678b17aa1bd
Jim Vallandingham <vlandham@gmail.com>
2015-03-27 09:10:50 -0700

    track backbone pages with GA

---
+++ src/js/shared/analytics.js
@@ -0,0 +1,26 @@
+define(function(require) {
+  var ga = require('ga');
+  function Analytics(options){
+    options = options || {};
+
+    ga(function() {
+      console.log('library done loading');
+    });
+  }
+
+  Analytics.prototype.trackPage = function(page) {
+    console.log(page);
+    ga("send", "pageview", page);
+  };
+
+  Analytics.prototype.trackEvent = function(category, action, label, count) {
+  };
+
+  // Return an instance so that this module is effectively
+  // a singleton providing a single source of truth.
+  //
+  // With require.js semantics this will also be run once so all
+  // requires of this module will get this one instance.
+  return new Analytics();
+
+});
