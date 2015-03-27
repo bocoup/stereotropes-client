00c83e0917bc507b6a41bc8e3ac9ed23458dcc4a
Irene Ros <imirene@gmail.com>
2015-03-27 17:10:50 -0400

    Revert "Making sure open new tab works for anchor tags"
    This reverts commit 669d44eb455e3ed3a73fbea1174a11b95d8973d6.

--- src/js/main.js
+++ src/js/main.js
@@ -6,41 +6,23 @@ define(function(require, exports, module) {
   var Router = require('./core/router');
 
   new Router();
-  var openLinkInTab = false;
 
   Backbone.history.start({
     pushState: true,
     root: "/"
   });
 
-  // making sure we can still open new tabs with backbone routes:
-  // https://gist.github.com/michaelkoper/9402728
+  // All navigation that is relative should be passed through the navigate
+  // method, to be processed by the router. If the link has a `data-bypass`
+  // attribute, bypass the delegation completely.
+  $(document).on("click", "a[href^='#']:not([data-bypass])", function(evt) {
+    // Prevent the default event (including page refresh).
+    evt.preventDefault();
 
-  $(document).keydown(function(event) {
-    if (event.ctrlKey || event.keyCode === 91) {
-      openLinkInTab = true;
-    }
-  });
-
-  $(document).keyup(function(event) {
-    openLinkInTab = false;
-  });
-
-  // Use delegation to avoid initial DOM selection and allow all matching elements to bubble
-  $(document).delegate("a", "click", function(evt) {
-    // Get the anchor href and protcol
+    // `Backbone.history.navigate` is sufficient for all Routers and will
+    // trigger the correct events. The Router's internal `navigate` method
+    // calls this anyways. The fragment is sliced from the root.
     var href = $(this).attr("href");
-    var protocol = this.protocol + "//";
-
-    // Ensure the protocol is not part of URL, meaning its relative.
-    // Stop the event bubbling to ensure the link will not cause a page refresh.
-    if (!openLinkInTab && href.slice(protocol.length) !== protocol) {
-      evt.preventDefault();
-
-      // Note by using Backbone.history.navigate, router events will not be
-      // triggered.  If this is a problem, change this to navigate on your
-      // router.
-      Backbone.history.navigate(href, true);
-    }
+    Backbone.history.navigate(href, true);
   });
 });
