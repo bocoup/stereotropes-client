9bd643c0ae1bb79888b1329dd61742fcdbf2d8ee
Irene Ros <imirene@gmail.com>
2015-03-30 17:16:45 -0400

    Updating nav & page rendering: - Nav now highlights page that it is on - Page remove always results in render of new page, even on fail.

--- src/js/shared/nav.js
+++ src/js/shared/nav.js
@@ -5,10 +5,24 @@ define(function(require) {
 
   return View.extend({
     template: template,
+
     _render: function() {
       this.$el.html(this.template());
       return this;
+    },
+
+    /**
+     * Update the selected page in the nav item.
+     * @param  {String} name Name of page to go to
+     * @return {View instance} Instance of nav.
+     */
+    page: function(name) {
+      this.$el.find('.inactive').removeClass('inactive');
+      this.$el.find('a[data-name=' + name + ']')
+        .addClass('inactive');
+      return this;
     }
+
   });
 
 });
