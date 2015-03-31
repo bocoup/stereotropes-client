235785fadb3c556d1886c2fb8bc763342118e85b
Irene Ros <imirene@gmail.com>
2015-03-30 22:27:36 -0400

    setting title on page change

--- src/js/shared/nav.js
+++ src/js/shared/nav.js
@@ -3,6 +3,12 @@ define(function(require) {
   var View = require('../core/view');
   var template = require('tmpl!../shared/nav');
 
+  function unCamelCase(s) {
+    // insert a space before all caps
+    return s.replace(/([A-Z])/g, ' $1')
+      // uppercase the first character
+      .replace(/^./, function(str){ return str.toUpperCase(); });
+  }
   return View.extend({
     template: template,
 
@@ -16,10 +22,19 @@ define(function(require) {
      * @param  {String} name Name of page to go to
      * @return {View instance} Instance of nav.
      */
-    page: function(name) {
+    page: function(name, options) {
       this.$el.find('.inactive').removeClass('inactive');
-      this.$el.find('a[data-name=' + name + ']')
+      var el = this.$el.find('a[data-name=' + name + ']')
         .addClass('inactive');
+      if (el.size()) {
+        document.title = "Stereotropes :: " + name;
+      } else if (name === 'film') {
+        document.title = "Stereotropes :: film - " + unCamelCase(options.film_id);
+      } else if (name === 'trope') {
+        document.title = "Stereotropes :: trope - " + unCamelCase(options.trope_id);
+      } else {
+        document.title = "Stereotropes";
+      }
       return this;
     }
 
