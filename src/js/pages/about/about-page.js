da6fb8357b7cf57b257bc32adae502640119f118
Irene Ros <imirene@gmail.com>
2015-04-08 15:41:26 -0400

    added press kit

--- src/js/pages/about/about-page.js
+++ src/js/pages/about/about-page.js
@@ -2,13 +2,22 @@ define(function(require) {
 
   var View = require('../../core/view');
   var template = require('tmpl!../about/about-page');
+  var Analytics = require("../../shared/analytics");
 
   return View.extend({
 
     template: template,
 
+    events: {
+      'click a.pk' : 'trackPK'
+    },
+
     initialize: function(options) {
 
+    },
+
+    trackPK: function() {
+      Analytics.trackEvent("about-page", "press-kit");
     },
 
     _render: function() {
